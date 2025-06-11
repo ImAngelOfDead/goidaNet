b8aa0e2f2e5ba9cca4a7706d8ce53d774d0d0670
silas <nkplkaa@gmail.com>
2025-06-11 19:48:05 +0300

    Initial Commit

---
+++ goida/goidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  goidaApp.swift
+//  goida
+//
+//  Created by silas on 11.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct goidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ goidaTests/goidaTests.swift
@@ -0,0 +1,17 @@
+//
+//  goidaTests.swift
+//  goidaTests
+//
+//  Created by silas on 11.06.2025.
+//
+
+import Testing
+@testable import goida
+
+struct goidaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
